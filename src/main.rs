15e9d6c42f9c1bbf4f5c7e93aded5de87eec1132
Elerphore <Elerphore@gmail.com>
2023-01-13 16:53:51 +0500

    chore: type convertion handling

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,39 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() -> io::Result<()> {
     println!("Guess the number!");
-    println!("Please input your guess!");
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
 
-    println!("Your guess: {guess}");
+    loop {
+        println!("Please input your guess!");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+
+        println!("Your guess: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("You've won!");
+                break;
+            },
+            Ordering::Greater => println!("Too big!"),
+        }
+    }
 
     Ok(())
 }
